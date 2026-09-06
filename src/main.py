9cece8057bb97610708ff70207fa7a5451ddd753
Maxwel-Araujo-Costa <82544832+Maxwel-Araujo-Costa@users.noreply.github.com>
2026-09-06 19:28:14 -0300

    refactor: separate data loading from dataframe processing.

--- src/main.py
+++ src/main.py
@@ -1,40 +1,52 @@
 from pathlib import Path
 import pandas as pd
-import json
+import argparse
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = BASE_DIR / "data"
-
+BASE_DIR = Path(__file__).resolve().parent.parent / "data"
 orders_path = BASE_DIR / "orders.csv"
 customers_path = BASE_DIR / "customers.json"
 
-def read_csv(csv_file):
+def read_csv(file_path):
     # Reads a CSV file and return a DataFrame
-    return pd.read_csv(csv_file)
+    return pd.read_csv(file_path)
 
-def read_json(json_file):
+def read_json(file_path):
     # Reads a JSON file and returns a Dataframe
-    with open(json_file, 'r', encoding='utf-8') as file:
-        json_data = json.load(file)
-    return pd.DataFrame(json_data)
+    return pd.read_json(file_path)
+
+def prepare_orders_dataframe(orders):
+    # Prepares the orders DataFrame by renaming columns, converting date to datetime, and ensuring value is numeric
+    orders = orders.rename(columns={'id': 'order_id'})
+    orders['date'] = pd.to_datetime(orders['date'])
+    orders['value'] = pd.to_numeric(orders['value'], errors='raise')
+    return orders
 
 def merge_dataframes(orders, customers):
     # Merges the orders and customers DataFrames on the customer_id and id columns
-    orders = read_csv(orders).rename(columns={'id': 'order_id'})
-    orders['date'] = pd.to_datetime(orders['date'])
-    if not pd.api.types.is_numeric_dtype(orders['value']):
-        orders['value'] = pd.to_numeric(orders['value'], errors='raise')
     return orders.merge(
-        read_json(customers),
+        customers,
         how='left',
         left_on='customer_id',
         right_on='id',
         validate='many_to_one').drop(columns='id')
 
-def main():
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Analyze customer orders.")
+    parser.add_argument("--start-date", type=pd.to_datetime, required=True)
+    parser.add_argument("--end-date", type=pd.to_datetime, required=True)
+    return parser.parse_args()
 
-    customers_orders = merge_dataframes(orders_path, customers_path)
-    print(customers_orders.head())
+def main():
+    args = parse_arguments()
+
+    orders = read_csv(orders_path)
+    customers = read_json(customers_path)
+
+    orders = prepare_orders_dataframe(orders)
+
+    orders_with_customers = merge_dataframes(orders, customers)
+
+
 
 if __name__ == "__main__":
     main()
